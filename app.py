c6d4f5d9c1c557f7f4cd239fdc1d79ff467e7d48
Thales-Melo <thales.penha@edu.ufes.br>
2025-03-19 22:12:20 -0300

    DEPLOY: Adaptado para a solução de Vargem Alta - Leia o README.md com as instruções

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from src.exibicao.exibicao import roda_app
 import subprocess
 
-# A função que a plataforma Shiny espera
-def app():
+def main():
     # Executa o comando "python ../main.py < input.txt" para baixar os dados necessários antes de iniciar o app
     subprocess.run("python src/main.py < input.txt", shell=True)
     
-    return roda_app()
+    roda_app()
 
-# Não é necessário o bloco if __name__ == "__main__" no app Shiny
+# Bloco principal
+if __name__ == "__main__":
+    main()
